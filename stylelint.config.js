306a70e9428c263784f7b851dcf666191766ae1d
Germán Freixinós <14282156+germanfrelo@users.noreply.github.com>
2025-01-20 11:25:55 +0100

    chore: improve CSS linting (#52)
    * Show list of checked files in Stylelint CLI output
    
    * Report disable comments that aren't scoped to rules
    
    * Remove too opinionated max and min rules
    
    * Remove rules already added to stylelint-config-standard in v37.0.0
    
    * Enable flex-wrapping rule from use-defensive-css plugin
    
    * Ignore -webkit-text-size-adjust property from the CSS reset
    
    * Add and clarify comments; reduce whitespace
    
    * Revert "Ignore -webkit-text-size-adjust property from the CSS reset"
    
    This reverts commit ab4eebdb643603b38246d75e6754ab20c71241f6.
    
    * Revert "Enable flex-wrapping rule from use-defensive-css plugin"
    
    This reverts commit 3ec46a80652d6fb5ba4a6245bb69f6f85d589792.

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,131 +1,60 @@
 /** @type {import('stylelint').Config} */
 export default {
 	extends: [
-		// Order matters: later configs take precedence over (override) earlier ones.
-		"stylelint-config-standard",
-		"stylelint-config-recess-order",
+		// The order of configs is important: later configs take precedence over earlier ones
+		"stylelint-config-standard", // Extends stylelint-config-recommended and turns on additional rules to enforce modern conventions
+		"stylelint-config-recess-order", // Sorts CSS properties
 	],
-	plugins: ["stylelint-plugin-defensive-css"],
+	plugins: ["stylelint-plugin-defensive-css"], // Enforces defensive CSS best practices
 	reportDescriptionlessDisables: true,
 	reportInvalidScopeDisables: true,
 	reportNeedlessDisables: true,
+	reportUnscopedDisables: true,
 	rules: {
-		/**
-		 * ----------------------------------------
-		 * Avoid errors
-		 * ----------------------------------------
-		 */
-
+		/* Avoid errors
+		---------------------------------------- */
 		// Descending
-		"no-descending-specificity": [true, { severity: "warning" }],
-
+		"no-descending-specificity": [
+			true, // Already enabled in stylelint-config-recommended
+			{ severity: "warning" }, // Default is 'error', but I prefer 'warning'
+		],
 		// Duplicate
 		"font-family-no-duplicate-names": [
-			true,
-			{ ignoreFontFamilyNames: ["monospace"] }, // See https://github.com/search?q=repo:germanfrelo/base-css-stylesheet "monospace, monospace"
+			true, // Already enabled in stylelint-config-recommended
+			{ ignoreFontFamilyNames: ["monospace"] }, // Don't report the 'font-family: monospace, monospace' declaration used in the CSS reset
 		],
-
 		// Unknown
-		"declaration-property-value-no-unknown": true,
-		"media-feature-name-value-no-unknown": true,
 		"no-unknown-animations": true,
 		"no-unknown-custom-media": true,
 
-		/**
-		 * ----------------------------------------
-		 * Enforce conventions
-		 * ----------------------------------------
-		 */
-
+		/* Enforce conventions
+		(overrides rules from stylelint-config-standard)
+		---------------------------------------- */
 		// Empty lines
 		"declaration-empty-line-before": "never",
-
-		// Max & Min
-		"max-nesting-depth": [
-			3,
-			{
-				ignore: ["blockless-at-rules", "pseudo-classes"],
-				severity: "warning",
-			},
-		],
-		"selector-max-attribute": [
-			3,
-			{
-				severity: "warning",
-			},
-		],
-		"selector-max-class": [
-			4,
-			{
-				severity: "warning",
-			},
-		],
-		"selector-max-compound-selectors": [
-			3,
-			{
-				severity: "warning",
-			},
-		],
-		"selector-max-id": [
-			2,
-			{
-				severity: "warning",
-			},
-		],
-		"selector-max-pseudo-class": [
-			3,
-			{
-				severity: "warning",
-			},
-		],
-		"selector-max-specificity": [
-			"2,4,3",
-			{
-				severity: "warning",
-			},
-		],
-		"selector-max-type": [
-			3,
-			{
-				severity: "warning",
-			},
-		],
-		"selector-max-universal": [
-			2,
-			{
-				severity: "warning",
-			},
-		],
-
 		// Notation
-		"media-feature-range-notation": null, // TODO: Remove when browser support is ~96% (https://caniuse.com/css-media-range-syntax)
-
+		"media-feature-range-notation": null, // TODO: Remove when 'baseline' is 'widely available' (~96%) (https://caniuse.com/css-media-range-syntax)
 		// Pattern
 		"custom-media-pattern": null,
 		"custom-property-pattern": null,
 		"keyframes-name-pattern": null,
 		"selector-class-pattern": null,
 		"selector-id-pattern": null,
-
 		// Redundant
 		"declaration-block-no-redundant-longhand-properties": null,
 
-		/**
-		 * ----------------------------------------
-		 * Plugin: use-defensive-css
-		 * ----------------------------------------
-		 */
-
+		/* Plugin: use-defensive-css
+		---------------------------------------- */
 		"plugin/use-defensive-css": [
 			true,
 			{
 				"severity": "warning",
-				"accidental-hover": false, // Unncecessary
-				"background-repeat": false, // Solved in assets/css/base.css
-				"custom-property-fallbacks": false, // TODO: Test it
-				"flex-wrapping": false, // Unncecessary
+				"accidental-hover": false, // Enable as needed
+				"background-repeat": false, // The imported CSS reset already apply 'no-repeat' to all elements
+				"custom-property-fallbacks": false, // Not suitable with WordPress
+				"flex-wrapping": false, // Not suitable with WordPress
 				"scroll-chaining": true,
-				"scrollbar-gutter": false, // TODO: Check browser support (~78% in Oct. 2024)
+				"scrollbar-gutter": false, // Enable as needed
 				"vendor-prefix-grouping": true,
 			},
 		],
