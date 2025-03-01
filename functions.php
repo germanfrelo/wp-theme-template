91d5ece0c9f2f585672711322bb5409519243049
Germán Freixinós <14282156+germanfrelo@users.noreply.github.com>
2025-03-01 19:04:40 +0100

    Improve and clarify comments and formatting, and refactor

--- functions.php
+++ functions.php
@@ -1,103 +1,111 @@
 <?php
 
-/**
- * IMPORTANT❗️:
- * Replace all instances of the word 'themeslug' with the name of your theme.
- */
+/* =============================================================================================
+❗️ IMPORTANT:
+Replace all instances of the word 'themeslug' with your theme name. Then remove this message.
+============================================================================================= */
 
 /**
- * Load stylesheets on the front end of the website.
+ * Theme's functions and definitions.
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ * @link https://developer.wordpress.org/themes/core-concepts/custom-functionality/
+ */
+
+
+/**
+ * Enqueues stylesheets on the front end.
  *
  * @link https://developer.wordpress.org/themes/core-concepts/including-assets/#front-end-stylesheets
- * @link https://developer.wordpress.org/themes/advanced-topics/child-themes/#loading-style-css
+ * @link https://developer.wordpress.org/reference/functions/wp_enqueue_style/
+ *
+ * @return void
  */
-
 function themeslug_enqueue_styles() {
-	// Load custom stylesheets
-	wp_enqueue_style( 'base-style', get_parent_theme_file_uri( 'assets/css/base.css' ) );
-	wp_enqueue_style( 'layouts-style', get_parent_theme_file_uri( 'assets/css/layouts.css' ) );
-	wp_enqueue_style( 'utility-classes-style', get_parent_theme_file_uri( 'assets/css/utility-classes.css' ) );
-	wp_enqueue_style( 'gravity-forms-style', get_parent_theme_file_uri( 'assets/css/gravity-forms.css' ) );
+	// Additional stylesheets.
+	wp_enqueue_style('themeslug-base', get_parent_theme_file_uri('assets/css/base.css'));
+	wp_enqueue_style('themeslug-layouts', get_parent_theme_file_uri('assets/css/layouts.css'));
+	wp_enqueue_style('themeslug-utility-classes', get_parent_theme_file_uri('assets/css/utility-classes.css'));
+	wp_enqueue_style('themeslug-gravity-forms', get_parent_theme_file_uri('assets/css/gravity-forms.css'));
 
-	// Load the theme's style.css file
-	wp_enqueue_style( 'themeslug-style', get_stylesheet_uri() );
+	// Active theme's style.css.
+	wp_enqueue_style('themeslug-style', get_stylesheet_uri());
 }
+add_action('wp_enqueue_scripts', 'themeslug_enqueue_styles');
 
-add_action( 'wp_enqueue_scripts', 'themeslug_enqueue_styles' );
 
 /**
- * Load stylesheets in the editor.
+ * Enqueues stylesheets in the editor.
  *
  * @link https://developer.wordpress.org/themes/core-concepts/including-assets/#editor-stylesheets
+ * @link https://developer.wordpress.org/reference/functions/add_editor_style/
+ *
+ * @return void
  */
-
-function themeslug_setup() {
-	add_editor_style( array(
-		// Load another custom stylesheets
-		get_parent_theme_file_uri( 'assets/css/base.css' ),
-		get_parent_theme_file_uri( 'assets/css/layouts.css' ),
-		get_parent_theme_file_uri( 'assets/css/utility-classes.css' ),
-		get_parent_theme_file_uri( 'assets/css/gravity-forms.css' ),
-
-		// Load the theme's style.css file
+function themeslug_editor_styles() {
+	add_editor_style(array(
+		// Additional stylesheets.
+		get_parent_theme_file_uri('assets/css/base.css'),
+		get_parent_theme_file_uri('assets/css/layouts.css'),
+		get_parent_theme_file_uri('assets/css/utilit-classes.css'),
+		get_parent_theme_file_uri('assets/css/gravity-forms.css'),
+		// Active theme's style.css.
 		get_stylesheet_uri()
-	) );
+	));
 }
+add_action('after_setup_theme', 'themeslug_editor_styles');
 
-add_action( 'after_setup_theme', 'themeslug_setup' );
 
 /**
- * Register custom block style variations.
+ * Registers custom block style variations.
  *
  * @link https://developer.wordpress.org/themes/features/block-style-variations/
  * @link https://developer.wordpress.org/reference/functions/register_block_style/
  */
-
 function themeslug_block_style_variations() {
 	// register_block_style(
 	// 	'core/_____',
 	// 	array(
 	// 		'name'         => '__________',
-	// 		'label'        => __( '__________', 'themeslug' ),
-	// 		'inline_style' => '
-	// 			.wp-block-_____.is-style-__________ {
-	// 				// Your block styles here...
-	// 			}
-	// 		'
+	// 		'label'        => __('__________', 'themeslug'),
 	// 	)
 	// );
 }
+add_action('init', 'themeslug_block_style_variations');
 
-add_action( 'init', 'themeslug_block_style_variations' );
 
 /**
- * Enqueue custom block stylesheets.
+ * Enqueues custom block stylesheets (on the front end and in the editor).
  *
+ * @link https://developer.wordpress.org/block-editor/how-to-guides/enqueueing-assets-in-the-editor/#theme-scripts-and-styles
  * @link https://developer.wordpress.org/themes/features/block-stylesheets/
  * @link https://developer.wordpress.org/news/2022/12/07/leveraging-theme-json-and-per-block-styles-for-more-performant-themes/
  * @link https://developer.wordpress.org/reference/functions/wp_enqueue_block_style/
+ *
+ * @return void
  */
-
 function themeslug_block_stylesheets() {
-	// Add the block name (with namespace) for each style
-	$blocks = array(
+	// Adds the block name (with namespace prefix) for each style.
+	$blocks = [
 		'core/button',
 		'core/navigation',
 		'core/query-pagination',
 		'core/site-logo',
-	);
+	];
 
-	// Loop through each block and enqueue its styles
-	foreach ( $blocks as $block ) {
-		// Replace slash with hyphen for filename
-		$slug = str_replace( '/', '-', $block );
+	// Loops through each block and enqueues its styles.
+	foreach ($blocks as $block) {
+		// Replaces slash with hyphen for filename.
+		$block_slug = str_replace('/', '-', $block);
 
-		wp_enqueue_block_style( $block, array(
-			'handle' => "themeslug-block-{$slug}",
-			'src'    => get_theme_file_uri( "assets/css/blocks/{$slug}.css" ),
-			'path'   => get_theme_file_path( "assets/css/blocks/{$slug}.css" )
-		) );
+		// Relative path of block stylesheets.
+		$blocks_path = "assets/css/blocks/{$block_slug}.css";
+
+		wp_enqueue_block_style($block, array(
+			'handle' => "themeslug-{$block_slug}",
+			'src'    => get_theme_file_uri($blocks_path),
+			'path'   => get_theme_file_path($blocks_path)
+		));
 	}
 }
-
-add_action( 'init', 'themeslug_block_stylesheets' );
+add_action('init', 'themeslug_block_stylesheets');
