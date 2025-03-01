a3a6ef37de766fd036824b4a553ce39f3adbe558
Germán Freixinós <14282156+germanfrelo@users.noreply.github.com>
2025-03-01 17:04:23 +0100

    Make Stylelint ignore -webkit-mask-* properties

--- stylelint.config.js
+++ stylelint.config.js
@@ -16,12 +16,12 @@ export default {
 		// Descending
 		"no-descending-specificity": [
 			true, // Already enabled in stylelint-config-recommended
-			{ severity: "warning" }, // Default is 'error', but I prefer 'warning'
+			{ severity: "warning" }, // Relax the severity level because this rule has limitations
 		],
 		// Duplicate
 		"font-family-no-duplicate-names": [
 			true, // Already enabled in stylelint-config-recommended
-			{ ignoreFontFamilyNames: ["monospace"] }, // Don't report the 'font-family: monospace, monospace' declaration used in the CSS reset
+			{ ignoreFontFamilyNames: ["monospace"] }, // Don't report the 'font-family: monospace, monospace' declaration used in base.css
 		],
 		// Unknown
 		"no-unknown-animations": true,
@@ -30,6 +30,11 @@ export default {
 		/* Enforce conventions
 		(overrides rules from stylelint-config-standard)
 		---------------------------------------- */
+		// Allowed, disallowed & required
+		"property-no-vendor-prefix": [
+			true, // Already enabled in stylelint-config-recommended
+			{ ignoreProperties: ["/^mask-/"] }, // Allow -webkit-mask-* properties
+		],
 		// Empty lines
 		"declaration-empty-line-before": "never",
 		// Notation
