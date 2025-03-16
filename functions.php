42471c21a01643315a8fea9421cfa67daccb818b
Germán Freixinós <14282156+germanfrelo@users.noreply.github.com>
2025-03-16 19:17:09 +0100

    Add templates, template parts, blocks stylesheets & styles

--- functions.php
+++ functions.php
@@ -48,6 +48,12 @@ function themeslug_enqueue_styles() {
 		wp_get_theme()->get('Version')
 	);
 
+	// WooCommerce custom stylesheet.
+	wp_register_style('themeslug-woocommerce', get_parent_theme_file_uri('assets/css/woocommerce.css'));
+	if (class_exists('woocommerce')) {
+		wp_enqueue_style('themeslug-woocommerce');
+	}
+
 	// Active theme's style.css.
 	wp_enqueue_style(
 		'themeslug-style',
@@ -57,6 +63,35 @@ function themeslug_enqueue_styles() {
 	);
 }
 add_action('wp_enqueue_scripts', 'themeslug_enqueue_styles');
+
+
+/**
+ * WooCommerce - Dequeues default stylesheets on the front end.
+ * Path of stylesheets: 'wp-content/plugins/woocommerce/assets/css/'
+ *
+ * @link https://woocommerce.com/document/disable-the-default-stylesheet/
+ * @see wp-content/plugins/woocommerce/includes/class-wc-frontend-scripts.php
+ */
+function themeslug_dequeue_wooocommerce_styles($enqueue_styles) {
+	// unset($enqueue_styles['woocommerce-blocktheme']);
+	// unset($enqueue_styles['woocommerce-general']);
+	// unset($enqueue_styles['woocommerce-layout']);
+	// unset($enqueue_styles['woocommerce-smallscreen']);
+
+	return $enqueue_styles;
+}
+add_filter('woocommerce_enqueue_styles', 'themeslug_dequeue_wooocommerce_styles');
+
+
+/**
+ * WooCommerce - Dequeues default blocks stylesheets on the front end.
+ *
+ * Inspect your site's source code <head> to find the exact handles of the styles.
+ */
+function themeslug_dequeue_woocommerce_blocks_styles() {
+	// wp_dequeue_style('wc-blocks-style');
+}
+add_action('wp_enqueue_scripts', 'themeslug_dequeue_woocommerce_blocks_styles', 100);
 
 
 /**
@@ -82,121 +117,45 @@ add_action('after_setup_theme', 'themeslug_editor_styles');
 
 
 /**
- * Enqueues custom block stylesheets (on the front end and in the Editor).
+ * Enqueues a stylesheet for each block (on the front end and in the editor), if it exists in the theme.
  *
- * @link https://developer.wordpress.org/reference/functions/wp_enqueue_block_style/
+ * @link https://developer.wordpress.org/block-editor/how-to-guides/enqueueing-assets-in-the-editor/#theme-scripts-and-styles
  * @link https://developer.wordpress.org/themes/features/block-stylesheets/
+ * @link https://developer.wordpress.org/news/2022/12/07/leveraging-theme-json-and-per-block-styles-for-more-performant-themes/
+ * @link https://markwilkinson.dev/code-snippets/enqueue-stylesheet-for-any-wordpress-block/
+ * @link https://developer.wordpress.org/reference/functions/wp_enqueue_block_style/
  *
  * @return void
  */
 function themeslug_block_stylesheets() {
-	// Adds the block name (with namespace prefix) for each style.
-	$blocks = [
-		"core/archives",
-		"core/audio",
-		"core/avatar",
-		"core/button",
-		"core/buttons",
-		"core/calendar",
-		"core/categories",
-		"core/code",
-		"core/column",
-		"core/columns",
-		"core/comment-author-name",
-		"core/comment-content",
-		"core/comment-date",
-		"core/comment-edit-link",
-		"core/comment-reply-link",
-		"core/comment-template",
-		"core/comments",
-		"core/comments-pagination",
-		"core/comments-pagination-next",
-		"core/comments-pagination-numbers",
-		"core/comments-pagination-previous",
-		"core/comments-title",
-		"core/cover",
-		"core/details",
-		"core/embed",
-		"core/file",
-		"core/gallery",
-		"core/group",
-		"core/heading",
-		"core/home-link",
-		"core/html",
-		"core/image",
-		"core/latest-comments",
-		"core/latest-posts",
-		"core/list",
-		"core/list-item",
-		"core/loginout",
-		"core/media-text",
-		"core/more",
-		"core/navigation",
-		"core/navigation-link",
-		"core/navigation-submenu",
-		"core/nextpage",
-		"core/page-list",
-		"core/page-list-item",
-		"core/paragraph",
-		"core/post-author",
-		"core/post-author-biography",
-		"core/post-author-name",
-		"core/post-comments-count",
-		"core/post-comments-form",
-		"core/post-comments-link",
-		"core/post-content",
-		"core/post-date",
-		"core/post-excerpt",
-		"core/post-featured-image",
-		"core/post-navigation-link",
-		"core/post-template",
-		"core/post-terms",
-		"core/post-time-to-read",
-		"core/post-title",
-		"core/preformatted",
-		"core/pullquote",
-		"core/query",
-		"core/query-no-results",
-		"core/query-pagination",
-		"core/query-pagination-next",
-		"core/query-pagination-numbers",
-		"core/query-pagination-previous",
-		"core/query-title",
-		"core/query-total",
-		"core/quote",
-		"core/read-more",
-		"core/rss",
-		"core/search",
-		"core/separator",
-		"core/shortcode",
-		"core/site-logo",
-		"core/site-tagline",
-		"core/site-title",
-		"core/social-link",
-		"core/social-links",
-		"core/spacer",
-		"core/table",
-		"core/table-of-contents",
-		"core/tag-cloud",
-		"core/template-part",
-		"core/term-description",
-		"core/verse",
-		"core/video"
-	];
+	// Gets all of the registered blocks
+	$blocks = WP_Block_Type_Registry::get_instance()->get_all_registered();
 
-	// Loops through each block and enqueues its styles.
-	foreach ($blocks as $block) {
-		// Replaces slash with hyphen for filename.
-		$block_slug = str_replace('/', '-', $block);
+	// If we have block names
+	if (!empty($blocks)) {
+		// Loop through each block and enqueue its styles
+		foreach ($blocks as $block) {
+			// Replaces slash with hyphen for filename
+			$block_slug = str_replace('/', '-', $block->name);
 
-		// Relative path of block stylesheets.
-		$blocks_path = "assets/css/blocks/{$block_slug}.css";
+			// Relative path of each block stylesheet
+			$block_path = "assets/css/blocks/{$block_slug}.css";
 
-		wp_enqueue_block_style($block, array(
-			'handle' => "themeslug-{$block_slug}",
-			'src'    => get_theme_file_uri($blocks_path),
-			'path'   => get_theme_file_path($blocks_path)
-		));
+			// If we have no file existing for this block, continue
+			if (!file_exists(get_theme_file_path($block_path))) {
+				continue;
+			}
+
+			// Enqueues the block stylesheet
+			wp_enqueue_block_style(
+				$block->name,
+				[
+					'handle' => "themeslug-{$block_slug}",
+					'src'    => get_theme_file_uri($block_path),
+					'path'   => get_theme_file_path($block_path)
+				]
+			);
+		}
 	}
 }
 add_action('init', 'themeslug_block_stylesheets');
@@ -220,3 +179,47 @@ function themeslug_block_style_variations() {
 	// );
 }
 add_action('init', 'themeslug_block_style_variations');
+
+
+/**
+ * WooCommerce - Declares theme support.
+ *
+ * @link https://developer.woocommerce.com/docs/template-structure-overriding-templates-via-a-theme/
+ * @link https://github.com/woocommerce/woocommerce/wiki/Declaring-WooCommerce-support-in-themes
+ *
+ * @return void
+ */
+function themeslug_add_woocommerce_support() {
+	add_theme_support('woocommerce');
+}
+add_action('after_setup_theme', 'themeslug_add_woocommerce_support');
+
+
+/**
+ * WooCommerce - Hides the Cart Link's text visually.
+ *
+ * This function uses the 'render_block' filter to intercept the rendering of all blocks.
+ * It specifically targets the 'woocommerce/cart-link' block and adds a 'visually-hidden'
+ * class to the span element containing the cart text, making it accessible to screen readers but
+ * visually hidden.
+ *
+ * @param string $block_content The HTML content of the rendered block.
+ * @param array  $block         An array containing information about the block (blockName, attributes, etc.).
+ *
+ * @return string The modified HTML content of the block.
+ */
+function themeslug_modify_cart_link_block($block_content, $block) {
+	if ('woocommerce/cart-link' === $block['blockName']) {
+		// Finds the span with class 'wc-block-cart-link__text' and add 'visually-hidden'.
+		$block_content = preg_replace(
+			'/(<span class="wc-block-cart-link__text">)/',
+			'$1<span class="visually-hidden">',
+			$block_content
+		);
+		// Ensures the closing 'span' is also correct.
+		$block_content = str_replace('<span class="wc-block-cart-link__text"><span class="visually-hidden">', '<span class="wc-block-cart-link__text visually-hidden">', $block_content);
+
+	}
+	return $block_content;
+}
+add_filter('render_block', 'themeslug_modify_cart_link_block', 10, 2);
