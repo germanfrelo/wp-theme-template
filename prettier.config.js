986dc752dbe175da7b770a78f86f898c0e97f325
Germán Freixinós <14282156+germanfrelo@users.noreply.github.com>
2025-07-04 10:15:30 +0200

    Import @wordpress/prettier-config in Prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -1,24 +1,21 @@
 // prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
 
+import wordpressPrettierConfig from "@wordpress/prettier-config";
+
 /**
  * @see https://prettier.io/docs/en/configuration.html
  * @type {import("prettier").Config}
  */
 const config = {
+	...wordpressPrettierConfig,
 	quoteProps: "consistent",
 	overrides: [
-		{
-			files: ["*.html"],
-			options: {
-				printWidth: 9999,
-			},
-		},
-		{
-			files: ["*.css"],
-			options: {
-				printWidth: 120,
-			},
-		},
+		// {
+		// 	files: ["*.css"],
+		// 	options: {
+		// 		printWidth: 120,
+		// 	},
+		// },
 		{
 			files: ["*.js", "*.jsx", "*.vue"],
 			options: {
