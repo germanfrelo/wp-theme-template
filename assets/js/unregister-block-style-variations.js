ef0930a85fe00b8d8717b22a07a9f9ea69b6fd12
Germán Freixinós <14282156+germanfrelo@users.noreply.github.com>
2025-08-07 22:37:20 +0200

    feat: Add WooCommerce files

--- assets/js/unregister-block-style-variations.js
+++ assets/js/unregister-block-style-variations.js
@@ -10,4 +10,5 @@ wp.domReady(() => {
 	wp.blocks.unregisterBlockStyle("core/quote", ["default", "plain"]);
 	wp.blocks.unregisterBlockStyle("core/site-logo", ["rounded"]);
 	wp.blocks.unregisterBlockStyle("core/separator", ["wide"]);
+	wp.blocks.unregisterBlockStyle("woocommerce/product-button", ["fill"]);
 });
