817ef725907c06d23c786cc8892a5f9e7c50d491
Germán Freixinós <14282156+germanfrelo@users.noreply.github.com>
2025-09-02 19:28:42 +0200

    WIP

--- inc/remove-wp-defaults.php
+++ inc/remove-wp-defaults.php
@@ -95,11 +95,11 @@ add_filter('wp_theme_json_data_default', 'themeslug_remove_wp_theme_json_default
  */
 function themeslug_remove_wp_block_styles() {
 	wp_dequeue_style('wp-block-categories');
-	wp_dequeue_style('wp-block-code');
+	// wp_dequeue_style('wp-block-code');
 	wp_dequeue_style('wp-block-details');
-	wp_dequeue_style('wp-block-list');
+	// wp_dequeue_style('wp-block-list');
 	wp_dequeue_style('wp-block-post-template');
-	wp_dequeue_style('wp-block-post-title');
+	// wp_dequeue_style('wp-block-post-title');
 	wp_dequeue_style('wp-block-quote');
 	wp_dequeue_style('wp-block-search');
 	wp_dequeue_style('wp-block-site-logo');
