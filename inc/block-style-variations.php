3781080b56193c1cb4e9358c0f42f5eaca415b9a
Germán Freixinós <14282156+germanfrelo@users.noreply.github.com>
2025-09-13 19:35:38 +0200

    style: Format block style variations file

--- inc/block-style-variations.php
+++ inc/block-style-variations.php
@@ -16,43 +16,71 @@
  */
 function themeslug_register_block_style_variations() {
 	register_block_style(
-		['core/button', 'core/read-more'], [
+		[
+			'core/button',
+			'core/read-more'
+		],
+		[
 			'name'  => 'outlined',
 			'label' => __( 'Outlined', 'themeslug' )
 		]
 	);
+
 	register_block_style(
-		['core/button', 'core/read-more'], [
+		[
+			'core/button',
+			'core/read-more'
+		],
+		[
 			'name'  => 'plain',
 			'label' => __( 'Plain', 'themeslug' )
 		]
 	);
+
 	register_block_style(
-		'core/details', [
+		[
+			'core/details'
+		],
+		[
 			'name'  => 'custom',
 			'label' => __( 'Custom', 'themeslug' )
 		]
 	);
 	register_block_style(
-		'core/list', [
+		[
+			'core/list'
+		],
+		[
 			'name'  => 'unordered-checkmark',
 			'label' => __( 'Unordered Checkmark', 'themeslug' )
 		]
 	);
+
 	register_block_style(
-		'core/list', [
+		[
+			'core/list'
+		],
+		[
 			'name'  => 'ordered-custom',
 			'label' => __( 'Ordered Custom', 'themeslug' )
 		]
 	);
+
 	register_block_style(
-		'core/navigation-link', [
+		[
+			'core/navigation-link'
+		],
+		[
 			'name'  => 'external',
 			'label' => __( 'External', 'themeslug' )
 		]
 	);
+
 	register_block_style(
-		'core/search', [
+		[
+			'core/search'
+		],
+		[
 			'name'  => 'direction-reversed',
 			'label' => __( 'Reversed direction', 'themeslug' )
 		]
